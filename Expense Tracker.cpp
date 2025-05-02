31831322f788e94712dd19dfcefafc68a0d50d96
UncleH25 <oneilljarryd@gmail.com>
2025-05-02 06:15:01 +0200

    Added ftsream and ofstream that will be used to read the data from expenses.txt, then create a new file called receipt.txt and write the data into it.

--- Expense Tracker.cpp
+++ Expense Tracker.cpp
@@ -9,6 +9,11 @@ int main()
 {
     //Variable
 	const double VAT_RATE = 0.15;
+
+	//Read the data in expenses.txt
+	fstream inData("expenses.txt", ios::in);
+	//Create a file called receipt.txt and write the data in it
+	ofstream outData("receipt.txt", ios::out);
     
 
     return 0;
